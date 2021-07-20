ae4942e44be338cf3e4f661cbf3499b4b6d01d82
CYBERDUDE3 <85865892+CYBERDUDE3@users.noreply.github.com>
2021-07-20 07:45:31 -0700

    Update script.py

--- script.py
+++ script.py
@@ -5,26 +5,15 @@ class script(object):
 
 You can call this as an Auto Filter Bot if you like :D
 
-This bot is exclusively made for @dcompanymoviess
+This bot is exclusively made for @M_STER_TECH
 
-You can see me in @dcompanymoviess <i>help</i></b>"""
-
-
-    HELP_MSG = """<b>How to use the bot??</b>
-<i>    Sorry you can't use me 😔,for movies join @trolldcompany
-</i>
-
-<b>© @Trolldcompany</b>"""
+C📲=> @M_STER_TECH"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : D C Filter Bot</b>
+    HELP_MSG = """<b>Sorry you can't use me 😔??</b>"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : M_STER_Filter Bot</b>
     
-⭕️<b>Creater :</b> @n_a_c_bot_developers
-
-⭕️<b>Language :</b> <code>Python3</code>
-
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>support group :</b> <a href='https://t.me/botsfor_you_bot'>click here</a> 
-
+⭕️<b>Creater :</b> @M_STER_TECH
 """
